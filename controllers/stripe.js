671da5fe429be16019299371feb918159a91e58d
Onesmus Nyakotyo <onyakotyo@yahoo.co.uk>
2022-04-19 12:19:26 +0400

    Update stripe.js

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -2,9 +2,7 @@
 
 import Stripe from 'stripe';
 
-//const stripeAPI = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-const stripeAPI = new Stripe('sk_test_DIeLvFU6F4MVTluMPrwu9lDL00OEVoDGsO');
+const stripeAPI = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export { stripeAPI }
 
